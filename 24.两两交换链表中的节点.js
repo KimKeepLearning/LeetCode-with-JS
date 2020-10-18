44ca5c8f56e3a2f8a47d741504a846f279c21817
Y.Huang <18261195537@163.com>
2020-10-18 15:58:36 +0800

    add 24

--- 24.两两交换链表中的节点.js
+++ 24.两两交换链表中的节点.js
@@ -16,9 +16,34 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
+var swapPairs = function (head) {
+    // 迭代
+    // if (!head || !head.next) return head;
+    // let dummyHead = new ListNode(-1);
+    // dummyHead.next = head;
+    // let p1 = head.next;
+    // let p2 = p1.next;
+    // let cur = dummyHead;
+    // while (cur.next && cur.next.next) {
+    //     p1 = cur.next;
+    //     p2 = cur.next.next;
+    //     cur.next = p2;
+    //     p1.next = p2.next;
+    //     p2.next = p1;
+    //     cur = p1; 
+    // }
+    // return dummyHead.next;
+
+    // 递归
     if (!head || !head.next) return head;
-    
+
+    let newHead = head.next;
+    head.next = swapPairs(newHead.next);
+    newHead.next = head;
+
+    return newHead;
+
 };
 // @lc code=end
+
 
