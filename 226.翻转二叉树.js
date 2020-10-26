28b5257e4390c0143f9c905f5802e5e4efbf3a66
Y.Huang <18261195537@163.com>
2020-10-26 15:39:20 +0800

    add 226

---
+++ 226.翻转二叉树.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (!root) return null;
+    let oldLeft = root.left;
+    let odlRight = root.right;
+    root.left = invertTree(odlRight);
+    root.right = invertTree(oldLeft);
+
+    return root;
+};
+// @lc code=end
+
