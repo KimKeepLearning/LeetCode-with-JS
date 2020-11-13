e91567157f823657346b24d0a6962cf7357f614d
Y.Huang <18261195537@163.com>
2020-11-13 20:23:08 +0800

    add 658 im tired exausted frustrated and feeling nothing

--- 658.找到-k-个最接近的元素.js
+++ 658.找到-k-个最接近的元素.js
@@ -13,8 +13,8 @@
  */
 var findClosestElements = function(arr, k, x) {
     // 没做出来
-    let low = 0, high = arr.length = 1;
-    let left = null, right = null;
+    let low = 0, high = arr.length - 1;
+    let res = [];
     while (low <= high) {
         let mid = Math.floor((low + high) / 2);
         if (arr[mid] === x) {
@@ -22,6 +22,27 @@ var findClosestElements = function(arr, k, x) {
         } else if (arr[mid] > x) high = mid - 1;
         else low = mid + 1;
     }
+    let ans = low;
+    let left = ans, right = ans;
+    while (right - left - 1 < k) {
+        if (left < 0) {
+            right++;
+            continue;
+        }
+        if (right >= arr.length) {
+            left--;
+            continue;
+        }
+        if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+            left--;
+        } else {
+            right++;
+        }
+    }
+    for (let i = left + 1; i < right; i++){
+        res.push(arr[i]);
+    }
+    return res;
 };
 // @lc code=end
 
