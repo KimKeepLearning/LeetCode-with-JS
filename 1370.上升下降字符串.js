230cc76e391e9d3b56c11b6009b93de9e646e9e1
Y.Huang <18261195537@163.com>
2020-11-20 19:34:54 +0800

    finished yesterday's 1370

--- 1370.上升下降字符串.js
+++ 1370.上升下降字符串.js
@@ -9,59 +9,90 @@
  * @param {string} s
  * @return {string}
  */
-var sortString = function(s) {
-    s = s.split('');
-    let flag = new Array(s.length).fill(0);
-    let res = [], lastVal = '', lastVal2 = 'z' + 1;
+var sortString = function (s) {
+    // 我的解法
+    // s = Array.from(s);
+    // let lastmin=undefined, lastmax=undefined;
+    // var findmin = () => {
+    //     // let temp1 = s;
+    //     s.sort();
+    //     let i = 0;
+    //     if (lastmin === undefined) {
+    //         lastmin = s[0];
+    //         s.splice(0, 1);
+    //         return lastmin;
+    //     } else {
+    //         while (s[i++] <= lastmin);
+    //         i--;
+    //         if (i < s.length) {
+    //             lastmin = s[i];
+    //             s.splice(i, 1);
+    //             return lastmin;
+    //         } else {
+    //             return undefined;
+    //         }
+    //     }
+        
+    // }
 
+    // var findmax = () => {
+    //     // let temp2 = s;
+    //     s.sort().reverse();
+    //     let j = 0;
+    //     if (lastmax === undefined) {
+    //         lastmax = s[0];
+    //         s.splice(0, 1);
+    //         return lastmax;
+    //     } else {
+    //         while (s[j++] >= lastmax);
+    //         j--;
+    //         if (j < s.length) {
+    //             lastmax = s[j];
+    //             s.splice(j, 1);
+    //             return lastmax;
+    //         } else {
+    //             return undefined;
+    //         }
+    //     }
+    // }
+    // let res = [];
+    // while (s.length) {
+    //     lastmin = undefined, lastmax = undefined;
+        
+    //     let tempres = findmin();
+    //     while (tempres) {
+    //         res.push(tempres);
+    //         tempres = findmin();
+    //     }
+    //     tempres = findmax();
+    //     while (tempres) {
+    //         res.push(tempres);
+    //         tempres = findmax();
+    //     }
+    // }
+    // return res.join('');
 
-    var findMin = function (s) {
-        let temps = s.sort();
-        let i = 0, len = temps.length;
-        while (i < len) {
-            if (temps[i] > lastVal && flag[i] === 0) {
-                flag[i] = 1;
-                lastVal = temps[i];
-                return temps[i];
-            } else i++;
+    // 找最大最小不必要写一个函数哦
+    s = s.split('').map(v => v.codePointAt());
+    let res = [];
+    while (s.length) {
+        let min = -Infinity, max = Infinity;
+    
+        while (true) {
+            min = Math.min(...s.filter(v => v > min));
+            if (min === Infinity) break;
+            res.push(min);
+            s.splice(s.indexOf(min), 1);
         }
-        return undefined;
+        
+        while (true) {
+            max = Math.max(...s.filter(v => v < max));
+            if (max === -Infinity) break;
+            res.push(max);
+            s.splice(s.indexOf(max), 1)
+        }
     }
-
-    var findMax = function (s) {
-        let temps2 = s.sort().reverse();
-        let i = 0, len2 = temps2.length;
-        console.log(flag);
-        while (i < len2) {
-            if (temps2[i] < lastVal2 && flag[i] === 0) {
-                flag[i] = 1;
-                lastVal2 = temps2[i];
-                return temps2[i];
-            } else i++;
-        }
-        return undefined;
-    }
-
-
-    while (flag.includes(0)) {
-        lastVal = '', lastVal2 = 'z' + 1;
-        let temRes = findMin(s);
-        // console.log(1, temRes);
-        while (temRes) {
-            res.push(temRes);
-            temRes = findMin(s);
-            // console.log(2, temRes);
-        }
-        temRes = findMax(s);
-        console.log(3, temRes);
-        while (temRes) {
-            res.push(temRes);
-            temRes = findMax(s);
-            // console.log(4, temRes);
-        }
-    }
-    // console.log(res);
-    return res.join('');
+    return res.map(v => String.fromCharCode(v)).join('');
 };
 
 // @lc code=end
